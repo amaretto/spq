c6613ae524bdda52b04e4c6d80be47635fc90c8f
amaretto <jabberwock26@gmail.com>
2022-04-12 22:22:25 +0900

    add checking args

--- query.go
+++ query.go
@@ -7,6 +7,16 @@ import (
 )
 
 func executeQuery(c *cli.Context) error {
-	fmt.Println("hoge")
+	var args = c.Args().Slice()
+	if len(args) == 0 {
+		return fmt.Errorf("no target args specified. see `spq query -h` for more details")
+	} else if len(args) > 1 {
+		return fmt.Errorf("too many args specified. see `spq query -h` for more details")
+	}
 	return nil
+}
+
+func query() {
+	// ToDo: implement
+	fmt.Println("exec query")
 }
