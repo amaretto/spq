6e470ab2e9a36b46415f1104a15078499c9a18ea
amaretto <jabberwock26@gmail.com>
2022-04-13 23:12:51 +0900

    WIP on authorization

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/amaretto/spq
 
 go 1.16
 
-require github.com/urfave/cli/v2 v2.4.0
+require (
+	github.com/urfave/cli/v2 v2.4.0
+	github.com/zmb3/spotify/v2 v2.0.1 // indirect
+)
--- query.go
+++ query.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/urfave/cli/v2"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
+const redirectURI = "http://localhost:27228/spotify_callback"
+
+var (
+	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+	ch    = make(chan *spotify.Client)
+	state = "hoge"
 )
 
 func executeQuery(c *cli.Context) error {
@@ -13,10 +26,46 @@ func executeQuery(c *cli.Context) error {
 	} else if len(args) > 1 {
 		return fmt.Errorf("too many args specified. see `spq query -h` for more details")
 	}
+	query()
 	return nil
 }
 
 func query() {
-	// ToDo: implement
-	fmt.Println("exec query")
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	client := <-ch
+
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
 }
