aed9720374b617ab5f16c767fa69db651d7cf8df
amaretto <jabberwock26@gmail.com>
2022-04-17 22:44:33 +0900

    wip

--- query.go
+++ query.go
@@ -29,27 +29,70 @@ func executeQuery(c *cli.Context) error {
 
 	// parse flags
 	artist := c.String("artist")
-	//album := c.String("album")
-	//track := c.String("track")
+	album := c.String("album")
+	track := c.String("track")
 
-	results, err := client.Search(ctx, artist, spotify.SearchTypeArtist|spotify.SearchTypeAlbum|spotify.SearchTypeTrack)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// handle album results
-	if results.Albums != nil {
-		fmt.Println("Albums:")
-		for _, item := range results.Albums.Albums {
-			fmt.Println("   ", item.Name)
+	if artist != "" {
+		results, err := client.Search(ctx, artist, spotify.SearchTypeArtist|spotify.SearchTypeAlbum|spotify.SearchTypeTrack)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if results.Artists != nil {
+			fmt.Println("Artists:")
+			for _, item := range results.Artists.Artists {
+				fmt.Println("    ", item.Name)
+			}
 		}
 	}
-	// handle playlist results
-	if results.Tracks != nil {
-		fmt.Println("Tracks:")
-		for _, item := range results.Tracks.Tracks {
-			fmt.Println("   ", item.Name)
+
+	if album != "" {
+		results, err := client.Search(ctx, album, spotify.SearchTypeAlbum)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if results.Albums != nil {
+			fmt.Println("albums:")
+			for _, item := range results.Albums.Albums {
+				artistString := ""
+				for i := 0; i < len(item.Artists); i++ {
+					artistString += item.Artists[i].Name
+				}
+				fmt.Println("artists:", artistString, "\t\t\talbum name:", item.Name)
+			}
 		}
 	}
+
+	if track != "" {
+		results, err := client.Search(ctx, track, spotify.SearchTypeTrack)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if results.Tracks != nil {
+			for _, item := range results.Tracks.Tracks {
+				artistString := ""
+				albumString := ""
+				for i := 0; i < len(item.Artists); i++ {
+					artistString += item.Artists[i].Name
+					albumString += item.Album.Name
+				}
+				fmt.Println("artists:", artistString, "\talbum:", albumString, "\ttrack:", item.Name)
+			}
+		}
+	}
+
+	//// handle album results
+	//if results.Albums != nil {
+	//	fmt.Println("Albums:")
+	//	for _, item := range results.Albums.Albums {
+	//		fmt.Println("   ", item.Name)
+	//	}
+	//}
+	//// handle playlist results
+	//if results.Tracks != nil {
+	//	fmt.Println("Tracks:")
+	//	for _, item := range results.Tracks.Tracks {
+	//		fmt.Println("   ", item.Name)
+	//	}
+	//}
 	return nil
 }
